36ece1e77b35c74a48b77aadfa944ffa3c41191e
Davronova <davronovamubina23@gmail.com>
2025-05-25 17:42:24 +0500

    status text

--- OnlineShopApp/models.py
+++ OnlineShopApp/models.py
@@ -95,14 +95,14 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField()
     product_code = models.PositiveIntegerField(unique=True, blank=True, null=True)
     status_type = [
-        ('mahsus_taklif', 'Mahsus taklif'),
-        ('yangilik', 'Yangilik'),
+        ('Mahsus taklif', 'mahsus_taklif'),
+        ('Yangilik', 'yangilik'),
     ]
     status = models.CharField(max_length=20, choices=status_type, default='yangilik', blank=True, null=True)
 
     status_type_ru = [
-        ('special', 'Специальное'),
-        ('new', 'Новое'),
+        ('Специальное', 'special'),
+        ('Новое', 'new'),
     ]
     status_ru = models.CharField(max_length=20, choices=status_type_ru, default='new', blank=True, null=True)
 
